0be1194e277a1507764d5626c9b3d9fefc732c71
hutdev <hutdevelopment@gmail.com>
2015-02-01 12:59:32 +0100

    Basic webservice

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 "use strict";
+var finalhandler = require('finalhandler'),
+    http = require('http'),
+    router = require('router')(),
+    url = require('url');
+
+router.post('/download', function (req, res) {
+    var query = url.parse(req.url, true).query,
+        validateQuery = function () {
+            return query
+                && query.get && query.get.match(/^http[s]?:\/\/.+$/)
+                && query.dest && query.dest.match(/^.+$/);
+        };
+
+    if (!validateQuery()) {
+        res.writeHead(400);
+    }
+    else {
+        console.log(query);
+    }
+    res.end();
+});
+
+http.createServer(function (req, res) {
+    router(req, res, finalhandler(req, res))
+}).listen(3000);
